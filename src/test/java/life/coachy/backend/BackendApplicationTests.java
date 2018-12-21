14ba051477370da7462b5b59bc6baee7d6a6a7ea
bmstefanski <bstefanskicom@gmail.com>
2018-12-21 14:48:46 +0100

    Add jococo + coveralls support (Closes #5)

--- src/test/java/life/coachy/backend/BackendApplicationTests.java
+++ src/test/java/life/coachy/backend/BackendApplicationTests.java
@@ -6,11 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class BackendApplicationTests {
+@SpringBootTest(classes = BackendApplication.class)
+class BackendApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
