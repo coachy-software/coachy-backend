6bf823ba9c499bdeb129e271da83b72695012921
bmstefanski <bstefanskicom@gmail.com>
2018-12-27 15:26:35 +0100

    Improve user repository integrations tests

--- src/test/java/life/coachy/backend/user/UserRepositoryIntegrationTest.java
+++ src/test/java/life/coachy/backend/user/UserRepositoryIntegrationTest.java
@@ -3,14 +3,14 @@ package life.coachy.backend.user;
 import com.google.common.collect.Sets;
 import java.util.Optional;
 import org.bson.types.ObjectId;
-import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @DataMongoTest
@@ -39,28 +39,11 @@ public class UserRepositoryIntegrationTest {
     Optional<User> mongoUser = this.userRepository.findByUsername("(nnu*SCA8=4{v::^Z_bagna]Gz(CUN");
 
     this.mongoTemplate.remove(user);
-    Assertions.assertEquals(mongoUser.get().toString(), user.toString());
-  }
 
-  @Test
-  public void storeUserShouldNotReturnProperValues() {
-    ObjectId identifier = ObjectId.get();
-    User user = new UserBuilder()
-        .withUsername("Q^&(gc<M+Zp52k`C,.x4kb5f)hTPK^")
-        .withPassword("test123")
-        .withEmail("test@coachy.life")
-        .withAvatar("http://www.coachy.life/some_avatar.jpg")
-        .withIdentifier(identifier)
-        .withAccountType(AccountType.CHARGE)
-        .withRoles(Sets.newHashSet("ADMIN", "USER"))
-        .build();
 
-    this.mongoTemplate.insert(user);
-    Optional<User> mongoUser = this.userRepository.findByUsername("Q^&(gc<M+Zp52k`C,.x4kb5f)hTPK^");
-
-    this.mongoTemplate.remove(user);
-    Assertions.assertThrows(AssertionError.class,
-        () -> Assert.assertNotEquals(mongoUser.get().toString(), user.toString())
+    assertAll(
+        () -> assertNotNull(mongoUser.get()),
+        () -> assertEquals(mongoUser.get().toString(), user.toString())
     );
   }
 
