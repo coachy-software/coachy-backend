d358453fb78d787325b5e831c79d514e6d0be41a
bmstefanski <bstefanskicom@gmail.com>
2019-01-04 20:09:44 +0100

    Fix auth module tests

--- src/main/java/life/coachy/backend/util/AbstractDto.java
+++ src/main/java/life/coachy/backend/util/AbstractDto.java
@@ -1,10 +1,12 @@
 package life.coachy.backend.util;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public abstract class AbstractDto<T extends IdentifiableEntity<?>> {
 
+  @JsonIgnore
   public String getName() {
     throw new UnsupportedOperationException();
   }
