409ce893c6e8bd7a8fdfb3b9fbd06a4d1170c66b
bmstefanski <bstefanskicom@gmail.com>
2019-01-05 09:27:58 +0100

    Set up swagger

---
+++ src/main/java/life/coachy/backend/swagger/SwaggerConfiguration.java
@@ -0,0 +1,35 @@
+package life.coachy.backend.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.DocumentationContextBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfiguration {
+
+  private static final String PATH_REGEX = "(?!.*error).*$";
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .paths(PathSelectors.regex(PATH_REGEX))
+        .build()
+        .apiInfo(new ApiInfoBuilder()
+            .title("Coachy API Docs")
+            .version("BETA")
+            .license("MIT")
+            .licenseUrl("https://github.com/coachy-software/coachy-backend/blob/master/LICENSE")
+            .contact(new Contact("Coachy", "http://coachy.life/", "support@coachy.life"))
+            .build()
+        );
+  }
+
+}
