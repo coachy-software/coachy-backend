48f136337f1bbf7a9704bb9473e49ca026528a8a
bmstefanski <bstefanskicom@gmail.com>
2019-01-24 11:48:30 +0100

    Move web mvc configuration to backend configuration

--- src/main/java/life/coachy/backend/BackendConfiguration.java
+++ src/main/java/life/coachy/backend/BackendConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 class BackendConfiguration {
@@ -36,4 +38,15 @@ class BackendConfiguration {
 @Profile("!test")
 class EmailConfiguration {
 
+}
+
+@Configuration
+class WebMvcConfiguration implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**")
+        .allowedOrigins("http://localhost:8080")
+        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
+        .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+  }
 }
