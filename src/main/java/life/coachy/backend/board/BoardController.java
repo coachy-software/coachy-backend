ad1b7e6a64c963158d5f4be830572f3e11a68081
bmstefanski <bstefanskicom@gmail.com>
2019-03-11 18:38:23 +0100

    Remove unnecessary imports

--- src/main/java/life/coachy/backend/board/BoardController.java
+++ src/main/java/life/coachy/backend/board/BoardController.java
@@ -1,15 +1,9 @@
 package life.coachy.backend.board;
 
-import com.querydsl.core.types.Predicate;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import life.coachy.backend.user.dto.UserUpdateDto;
 import life.coachy.backend.util.AbstractCrudController;
-import life.coachy.backend.util.PredicateResponseFactory;
 import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
