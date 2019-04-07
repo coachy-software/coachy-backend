d9ecf8deda40c39f4018b7a18b0476c457733295
bmstefanski <bstefanskicom@gmail.com>
2019-04-07 15:41:32 +0200

    Remove exercise template domain model and repository

--- src/main/java/life/coachy/backend/exercise/template/domain/ExerciseTemplateRepository.java
+++
@@ -1,8 +0,0 @@
-package life.coachy.backend.exercise.template.domain;
-
-import life.coachy.backend.infrastructure.command.CommandRepository;
-import org.bson.types.ObjectId;
-
-interface ExerciseTemplateRepository extends CommandRepository<ExerciseTemplate, ObjectId> {
-
-}
