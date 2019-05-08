ad933bfad21f8024536ca0fed8f7fb6cc7ea0df2
bmstefanski <bstefanskicom@gmail.com>
2019-05-08 22:01:15 +0200

    Remove redundant email exception

--- src/main/java/life/coachy/backend/email/exception/EmailNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package life.coachy.backend.email.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Email not found")
-public class EmailNotFoundException extends RuntimeException {
-
-}
