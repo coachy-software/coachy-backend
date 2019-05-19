9f7a5c95a25c1e0f0c2203f04c3b9cc10593155d
bmstefanski <bstefanskicom@gmail.com>
2019-05-19 19:47:38 +0200

    Update CORS allowed origin URL

--- src/main/java/life/coachy/backend/infrastructure/config/WebMvcConfiguration.java
+++ src/main/java/life/coachy/backend/infrastructure/config/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package life.coachy.backend.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 class WebMvcConfiguration implements WebMvcConfigurer {
 
+  @Value("${frontend.uri}") private String allowedOrigins;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
         .allowCredentials(true)
-        .allowedOrigins("http://localhost:8080")
+        .allowedOrigins(this.allowedOrigins)
         .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
         .allowedHeaders("Authorization", "Cache-Control", "Content-Type")
         .exposedHeaders("Location");
