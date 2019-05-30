4804bd839a6ef00bf3aa28f136ad34cb8ffcb541
bmstefanski <bstefanskicom@gmail.com>
2019-05-30 16:22:07 +0200

    Notification rest layer

--- src/main/java/life/coachy/backend/notification/NotificationEndpoint.java
+++ src/main/java/life/coachy/backend/notification/NotificationEndpoint.java
@@ -1,6 +1,19 @@
 package life.coachy.backend.notification;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import life.coachy.backend.infrastructure.authentication.RequiresAuthenticated;
 import life.coachy.backend.infrastructure.constant.ApiLayers;
+import life.coachy.backend.infrastructure.permission.RequiresPermissions;
+import life.coachy.backend.notification.domain.NotificationFacade;
+import life.coachy.backend.notification.query.NotificationQueryDto;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(ApiLayers.NOTIFICATIONS)
 class NotificationEndpoint {
 
+  private final NotificationFacade notificationFacade;
 
+  @Autowired
+  NotificationEndpoint(NotificationFacade notificationFacade) {
+    this.notificationFacade = notificationFacade;
+  }
+
+  @ApiOperation("Displays all notifications belonging to specified user's id")
+  @RequiresPermissions("user.{id}.read")
+  @RequiresAuthenticated
+  @GetMapping("{recipientId}")
+  Page<NotificationQueryDto> fetchAllByRecipientId(@PathVariable @ApiParam("Notification's recipient id") ObjectId recipientId, @PageableDefault(size = 5) Pageable pageable) {
+    return this.notificationFacade.fetchAllByRecipientId(recipientId, pageable);
+  }
 
 }
