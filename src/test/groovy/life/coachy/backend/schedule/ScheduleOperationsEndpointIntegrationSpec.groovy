de228b126188bf098c8690c83ef142c90c9ac558
bmstefanski <bstefanskicom@gmail.com>
2019-06-06 16:04:55 +0200

    Schedule module integration tests improvements

--- src/test/groovy/life/coachy/backend/schedule/ScheduleOperationsEndpointIntegrationSpec.groovy
+++ src/test/groovy/life/coachy/backend/schedule/ScheduleOperationsEndpointIntegrationSpec.groovy
@@ -1,5 +1,6 @@
 package life.coachy.backend.schedule
 
+import com.google.common.collect.Sets
 import life.coachy.backend.base.IntegrationSpec
 import life.coachy.backend.infrastructure.converter.ObjectToJsonConverter
 import org.bson.types.ObjectId
@@ -11,7 +12,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-class ScheduleOperationsEndpointIntegrationSpec extends IntegrationSpec {
+class ScheduleOperationsEndpointIntegrationSpec extends IntegrationSpec implements SampleSchedules {
 
   @Autowired ObjectToJsonConverter jsonConverter;
 
@@ -29,4 +30,17 @@ class ScheduleOperationsEndpointIntegrationSpec extends IntegrationSpec {
       acceptEndpoint.andExpect(status().isForbidden())
   }
 
+  def "'accept' endpoint should return 400 if payload hasn't been provided"() {
+    given: "we have one schedule and user in system"
+      def user = setUpUser(ObjectId.get(), "yang160", "password123", Sets.newHashSet("schedule.${id}.accept"))
+      def schedule = setUpSchedule(id, "test", user.get("_id"), user.get("_id"))
+      def request = setUpRequest(user.get("_id"))
+    when: "I post to /api/schedules/{id}/accept"
+      ResultActions acceptEndpoint = mockMvc.perform(post('/api/schedules/{id}/accept', schedule.get("_id"))
+          .with(httpBasic('yang160', 'password123'))
+          .content(jsonConverter.convert(Collections.singletonMap("wrongToken", request.get("token"))))
+          .contentType(MediaType.APPLICATION_JSON))
+    then:
+      acceptEndpoint.andExpect(status().isBadRequest())
+  }
 }
