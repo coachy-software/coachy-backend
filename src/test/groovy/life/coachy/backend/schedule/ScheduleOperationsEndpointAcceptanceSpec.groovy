4875a2ee9b02485525eee1b5454cb0eb52032ff7
bmstefanski <bstefanskicom@gmail.com>
2019-06-06 15:48:39 +0200

    Add another acceptance test scenario to schedule module and fix travis build

--- src/test/groovy/life/coachy/backend/schedule/ScheduleOperationsEndpointAcceptanceSpec.groovy
+++ src/test/groovy/life/coachy/backend/schedule/ScheduleOperationsEndpointAcceptanceSpec.groovy
@@ -7,6 +7,7 @@ import org.json.JSONArray
 import org.json.JSONObject
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.IfProfileValue
 import org.springframework.test.web.servlet.ResultActions
 
 import static org.hamcrest.Matchers.is
@@ -19,6 +20,7 @@ class ScheduleOperationsEndpointAcceptanceSpec extends IntegrationSpec implement
 
   @Autowired ObjectToJsonConverter jsonConverter;
 
+  @IfProfileValue(name = "spring.profiles.active", value = "nonCI")
   def "'accept' endpoint positive scenario"() {
     given: "I have one user in system"
       setUpUser(sampleCreateDto.getCharge(), "yang160", "password123", Sets.newHashSet("user.${sampleCreateDto.getCharge()}.read"))
@@ -51,6 +53,34 @@ class ScheduleOperationsEndpointAcceptanceSpec extends IntegrationSpec implement
           .contentType(MediaType.APPLICATION_JSON))
     then: "should accept the schedule"
       acceptEndpoint.andExpect(status().isOk())
+  }
+
+  @IfProfileValue(name = "spring.profiles.active", value = "nonCI")
+  def "'accept' endpoint negative scenario"() {
+    given: "I have one user in system"
+      setUpUser(sampleCreateDto.getCharge(), "yang160", "password123", Sets.newHashSet("user.${sampleCreateDto.getCharge()}.read"))
+    when: "I post to /api/schedules"
+      ResultActions createEndpoint = mockMvc.perform(post('/api/schedules')
+          .with(httpBasic('yang160', 'password123'))
+          .content(jsonConverter.convert(sampleCreateDto))
+          .contentType(MediaType.APPLICATION_JSON))
+    then: "should create the schedule"
+      createEndpoint.andExpect(status().isCreated())
+          .andExpect(header().exists("Location"))
+    when: "I go to /api/schedules/{id}"
+      ResultActions detailsEndpoint = mockMvc.perform(get(createEndpoint.andReturn().getResponse().getRedirectedUrl())
+          .with(httpBasic("yang160", "password123")))
+    then: "I see schedule details"
+      detailsEndpoint.andExpect(status().isOk())
+          .andExpect(jsonPath('$.note', is("brief note")))
+      def scheduleId = new JSONObject(detailsEndpoint.andReturn().getResponse().getContentAsString()).get("identifier");
+    when: "I post to /api/schedules/{id}/accept"
+      ResultActions acceptEndpoint = mockMvc.perform(post('/api/schedules/{scheduleId}/accept', scheduleId)
+          .with(httpBasic("yang160", "password123"))
+          .content(jsonConverter.convert(Collections.singletonMap("token", "wrong token")))
+          .contentType(MediaType.APPLICATION_JSON))
+    then:
+      acceptEndpoint.andExpect(status().isNotFound())
   }
 
 }
